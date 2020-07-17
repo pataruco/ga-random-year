e52c9d21c533a91cab38dd6d3cd0e4f6ef0c8896
Pedro Martin <pedro.martin@red-badger.com>
2020-07-17 14:36:45 +0100

    Refactor webpack config

--- config/webpack.config.prod.ts
+++ config/webpack.config.prod.ts
@@ -1,76 +1,45 @@
 import FaviconsWebpackPlugin from 'favicons-webpack-plugin';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin';
 import path from 'path';
 import TerserJSPlugin from 'terser-webpack-plugin';
 import webpack from 'webpack';
-import { favicon } from './webpack.config.dev';
+import baseConfig, { favicon } from './webpack.config.base';
+
+const plugins = baseConfig.plugins?.concat(
+  new FaviconsWebpackPlugin({
+    logo: favicon,
+    favicons: {
+      icons: {
+        android: false,
+        appleIcon: false,
+        appleStartup: false,
+        coast: false,
+        favicons: true,
+        firefox: false,
+        yandex: false,
+        windows: false,
+      },
+    },
+  }),
+  new webpack.IgnorePlugin({
+    resourceRegExp: /^\.\/locale$/,
+    contextRegExp: /moment$/,
+  }),
+);
 
 const config: webpack.Configuration = {
-  entry: './src/index.ts',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, '../build'),
-  },
+  ...baseConfig,
   mode: 'production',
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      { test: /\.ts$/, exclude: /node_modules/, loader: 'ts-loader' },
-      {
-        test: /\.svg$/,
-        loader: 'svg-url-loader',
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'GA random year picker',
-      filename: 'index.html',
-      template: 'src/index.html',
-    }),
-    new HtmlWebpackPlugin({
-      title: 'GA año aleatorio',
-      filename: 'es/index.html',
-      template: 'src/es/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: './[name].css',
-    }),
-    new FaviconsWebpackPlugin({
-      logo: favicon,
-      favicons: {
-        icons: {
-          android: false,
-          appleIcon: false,
-          appleStartup: false,
-          coast: false,
-          favicons: true,
-          firefox: false,
-          yandex: false,
-          windows: false,
-        },
-      },
-    }),
-    new webpack.IgnorePlugin({
-      resourceRegExp: /^\.\/locale$/,
-      contextRegExp: /moment$/,
-    }),
-  ],
   optimization: {
     minimizer: [
       new TerserJSPlugin({ extractComments: true }),
       new OptimizeCSSAssetsPlugin({}),
     ],
   },
+  output: {
+    ...baseConfig.output,
+    path: path.resolve(__dirname, '../build'),
+  },
+  plugins,
 };
-
 export default config;
